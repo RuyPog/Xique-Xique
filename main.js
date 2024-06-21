0f5707900bd8fec5d5dab53d6beffcb3623c8fd5
RuyPog <110060905+RuyPog@users.noreply.github.com>
2024-06-21 09:41:55 -0300

    21-06

--- main.js
+++ main.js
@@ -1,6 +1,25 @@
 const botoes  = document.querySelectorAll(".botao");
 const conteudo  = document.querySelectorAll(".aba-conteudo");
 const tempo = document.querySelectorAll(".contador");
+ tempo[0]. textContent = "Olá";
+const tempoObjetivo1 = new Date(2024,12,13);
+const tempoObjetivo2 = new Date(2024,12,02);
+const agora = new Date();
+let segundos ;
+segundos = (tempoObjetivo1 - agora)/1000;
+segundos = Math.floor(segundos);
+let minutos;
+minutos = segundos/60;
+minutos = Math.floor(minutos);
+let horas;
+horas = minutos/60;
+horas = Math.floor(horas);
+let dias;
+dias = horas/24;
+dias = Math.floor(dias);
+tempo[1].  textContent = agora;
+tempo[0]. textContent = tempoObjetivo1 - agora;
+tempo[0].textContent = `faltam ${dias}dias, ${horas}horas, ${minutos}minutos e ${segundos}segundos`;
 for (let i = 0; i < botoes.length ; i++){
     botoes[i].onclick = function(){
     for (j=0;j<botoes.length;j++){
